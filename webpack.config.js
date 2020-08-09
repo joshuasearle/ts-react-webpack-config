0bda558bf151b2bacd0a4a2e500ad90724a1fcec
joshuasearle <joshryansearle@gmail.com>
2020-08-09 12:41:48 +1000

    Configure css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
